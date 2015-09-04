83c5931198817a80b407b3235fc57076fae1612e
sambmish <sambmish@gmail.com>
2015-09-04 22:22:32 +0530

    refactored code

--- StudentApp/src/com/ali/client/MainPage.java
+++ StudentApp/src/com/ali/client/MainPage.java
@@ -1,119 +1,51 @@
 package com.ali.client;
 
-import com.google.gwt.dom.client.TableCellElement;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.layout.client.Layout.Alignment;
-import com.google.gwt.thirdparty.guava.common.collect.Table;
 import com.google.gwt.user.client.ui.Anchor;
-import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
-import com.google.gwt.user.client.ui.HasHorizontalAlignment.HorizontalAlignmentConstant;
 import com.google.gwt.user.client.ui.HorizontalPanel;
-import com.google.gwt.user.client.ui.Label;
-import com.google.gwt.user.client.ui.ListBox;
-import com.google.gwt.user.client.ui.PopupPanel;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
-import com.sun.corba.se.impl.interceptors.PICurrent;
 
 public class MainPage extends Composite {
 	VerticalPanel vpanel = new VerticalPanel();
-	HorizontalPanel hp=new HorizontalPanel();
+	HorizontalPanel hp = new HorizontalPanel();
 	TextBox srchNametb;
 	TextBox srchRNtb;
 	TextBox srchStrdtb;
-	
+
 	TextBox searchtb;
-	public MainPage(){
+
+	public MainPage() {
 		initWidget(vpanel);
-		
-		Anchor addStudent=new Anchor("Add Student");
-		Anchor searchStudent=new Anchor("Search/Modify Student");
-	
+
+		Anchor addStudent = new Anchor("Add Student");
+		Anchor searchStudent = new Anchor("Search/Modify Student");
+
 		addStudent.addClickHandler(new AddStudentClickHandler());
 		searchStudent.addClickHandler(new SearchStudentClickHandler());
-		
+
 		RootPanel.get().clear();
 		vpanel.add(addStudent);
 		vpanel.add(searchStudent);
 		RootPanel.get().add(this);
 	}
-	public class AddStudentClickHandler implements ClickHandler{
-		@Override
-		public void onClick(ClickEvent event) {
-			// TODO Auto-generated method stub
-			
-			AddStudent addStudentpage=new AddStudent();
-			
-			//vpanel.add(addStudentpage);
-			
-		}
-	}
-	public class SearchStudentClickHandler implements ClickHandler{
-		@Override
-		public void onClick(ClickEvent event) {
-			// TODO Auto-generated method stub
-			RootPanel.get().clear();
-			VerticalPanel vp=new VerticalPanel();
-			 
-			
-			HorizontalPanel srchNamehp=new HorizontalPanel();
-			HorizontalPanel srchRNhp=new HorizontalPanel();
-			HorizontalPanel srchStrdhp=new HorizontalPanel();
-			
-			Label srchNameLbl = new Label("Name:");
-			Label srchRNLbl = new Label("Roll No:");
-			Label srchStrdLbl = new Label("Standard:");
-			
-			srchNametb=new TextBox();
-			srchRNtb=new TextBox();
-			srchStrdtb=new TextBox();
-			
-			srchNamehp.add(srchNameLbl);
-			srchNamehp.add(srchNametb);
-			srchRNhp.add(srchRNLbl);
-			srchRNhp.add(srchRNtb);
-			srchStrdhp.add(srchStrdLbl);
-			srchStrdhp.add(srchStrdtb);
-			
-			Button search=new Button("Search");
-			search.addClickHandler(new GoBttonHandler());
-			
-			vp.add(srchNamehp);
-			vp.add(srchRNhp);
-			vp.add(srchStrdhp);
-			
-			vp.add(search);
-			
-			vp.setSize("300px", "100px");
-			
-			
-			RootPanel.get().add(vp);
-			
-			//vpanel.add(addStudentpage);
-			
-		}
-	}
-	public class GoBttonHandler implements ClickHandler{
-		@Override
-		public void onClick(ClickEvent event) {
-			// TODO Auto-generated method stub
-			
-					
-					String name=srchNametb.getValue();
-					String rollNo=srchRNtb.getValue();
-					String strd=srchStrdtb.getValue();
-					
-					StudentSearchResult searcResult=new StudentSearchResult(name,rollNo,strd);
-					RootPanel.get().add(searcResult);
-					
-				}
-			
-			//vpanel.add(addStudentpage);
-			
-		}
-	
 
+	public class AddStudentClickHandler implements ClickHandler {
+		@Override
+		public void onClick(ClickEvent event) {
+			RootPanel.get().clear();
+			RootPanel.get().add(new AddStudent());
+		}
+	}
+
+	public class SearchStudentClickHandler implements ClickHandler {
+		@Override
+		public void onClick(ClickEvent event) {
+			RootPanel.get().clear();
+			RootPanel.get().add(new StudentSearchResult());
+		}
+	}
 }
--- StudentApp/src/com/ali/client/StudentSearchResult.java
+++ StudentApp/src/com/ali/client/StudentSearchResult.java
@@ -1,11 +1,7 @@
 package com.ali.client;
 
-import java.awt.GridLayout;
-import java.util.Date;
-import java.util.Iterator;
 import java.util.List;
 
-import com.ali.client.AddStudent.BackButtonClickHandler;
 import com.ali.shared.Student;
 import com.google.gwt.cell.client.CheckboxCell;
 import com.google.gwt.cell.client.EditTextCell;
@@ -14,16 +10,13 @@ import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.cellview.client.Column;
 import com.google.gwt.user.cellview.client.DataGrid;
-import com.google.gwt.user.cellview.client.HasKeyboardSelectionPolicy.KeyboardSelectionPolicy;
-import com.google.gwt.user.cellview.client.TextColumn;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.CheckBox;
 import com.google.gwt.user.client.ui.Composite;
-import com.google.gwt.user.client.ui.Grid;
 import com.google.gwt.user.client.ui.HorizontalPanel;
-import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.SubmitButton;
+import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
 public class StudentSearchResult extends Composite {
@@ -37,154 +30,191 @@ public class StudentSearchResult extends Composite {
 	private final StudentServiceAsync studentService = GWT
 			.create(StudentService.class);
 	VerticalPanel vpanel = new VerticalPanel();
-	VerticalPanel vpanel1 = new VerticalPanel();
-	DataGrid<Student> studentgrid = new DataGrid<Student>();
-	List<Student> result1;
-	public StudentSearchResult(String name,String rollNo,String standard ) {
+	TextBox srchNametb = new TextBox();
+	TextBox srchRNtb = new TextBox();
+	TextBox srchStrdtb = new TextBox();
+	VerticalPanel headerPanel = new VerticalPanel();
+
+	public class GoBttonHandler implements ClickHandler {
+		@Override
+		public void onClick(ClickEvent event) {
+			String name = srchNametb.getValue();
+			String rollNo = srchRNtb.getValue();
+			String strd = srchStrdtb.getValue();
+			search(name, rollNo, strd);
+		}
+	}
+
+	public void initializePage() {
+		HorizontalPanel srchNamehp = new HorizontalPanel();
+		HorizontalPanel srchRNhp = new HorizontalPanel();
+		HorizontalPanel srchStrdhp = new HorizontalPanel();
+
+		Label srchNameLbl = new Label("Name:");
+		Label srchRNLbl = new Label("Roll No:");
+		Label srchStrdLbl = new Label("Standard:");
+
+		srchNamehp.add(srchNameLbl);
+		srchNamehp.add(srchNametb);
+		srchRNhp.add(srchRNLbl);
+		srchRNhp.add(srchRNtb);
+		srchStrdhp.add(srchStrdLbl);
+		srchStrdhp.add(srchStrdtb);
+
+		Button search = new Button("Search");
+		search.addClickHandler(new GoBttonHandler());
+
+		headerPanel.add(srchNamehp);
+		headerPanel.add(srchRNhp);
+		headerPanel.add(srchStrdhp);
+
+		headerPanel.add(search);
+
+		headerPanel.setSize("300px", "100px");
+		vpanel.add(headerPanel);
+	}
+
+	public StudentSearchResult() {
 		initWidget(vpanel);
+		initializePage();
+	}
 
-		studentService.searchStudent(name,rollNo,standard, new AsyncCallback<List<Student>>() {
+	private void search(String name, String rollNo, String standard) {
+		studentService.searchStudent(name, rollNo, standard,
+				new AsyncCallback<List<Student>>() {
 
-			@Override
-			public void onFailure(Throwable caught) {
-				// TODO Auto-generated method stub
-
-			}
-
-			@Override
-			public void onSuccess(List<Student> result) {
-				// TODO Auto-generated method stub
-				addResult(result);
-
-			}
-
-			private void addResult(List<Student> result) {
-				// TODO Auto-generated method stub
-				result1=result;
-				HorizontalPanel hpanel=new HorizontalPanel();
-				SubmitButton backbutton=new SubmitButton("Back");
-				//backbutton.setWidth("1");
-				backbutton.addClickHandler(new BackButtonClickHandler());
-				hpanel.add(backbutton);
-				vpanel.add(hpanel);
-				
-				
-				studentgrid.setRowData(result);
-				 Column<Student, Boolean> checkBoxColumn = new Column<Student, Boolean>(
-				            new CheckboxCell(false, false)) {
-						@Override
-						public Boolean getValue(Student object) {
-							// TODO Auto-generated method stub
-							return null;
-						}
-				    };
-				    
-				    
-					EditTextCell nameedit=new EditTextCell();
-					Column<Student,String> namecolumn=new Column<Student,String>(nameedit) {
-						
-						@Override
-						public String getValue(Student object) {
-							// TODO Auto-generated method stub
-							return object.getName();
-						}
-					};
-					EditTextCell rnedit=new EditTextCell();
-					Column<Student,String> rncolumn=new Column<Student,String>(rnedit) {
-						
-						@Override
-						public String getValue(Student object) {
-							// TODO Auto-generated method stub
-							return Integer.toString(object.getRollNo());
-						}
-					};
-				
-					EditTextCell dobedit=new EditTextCell();
-					Column<Student,String> dobcolumn=new Column<Student,String>(dobedit) {
-						
-						@Override
-						public String getValue(Student object) {
-							// TODO Auto-generated method stub
-							return object.getDob().toString();
-						}
-					};
-				
-					EditTextCell classedit=new EditTextCell();
-					Column<Student,String> classcolumn=new Column<Student,String>(classedit) {
-						
-						@Override
-						public String getValue(Student object) {
-							// TODO Auto-generated method stub
-							return object.getClass_();
-						}
-					};
-				studentgrid.addColumn(checkBoxColumn,"Select");
-				studentgrid.addColumn(namecolumn, "Name");
-				studentgrid.addColumn(rncolumn, "Roll No");
-				studentgrid.addColumn(dobcolumn, "Date Of Birth");
-				studentgrid.addColumn(classcolumn, "Standard");
-				studentgrid.setSize("400px", "400px");
-				
-				HorizontalPanel h1=new HorizontalPanel();
-				RootPanel.get().remove(vpanel1);
-				vpanel1.add(studentgrid);
-				vpanel1.setBorderWidth(1);
-				vpanel.add(vpanel1);
-				Button updatebutton=new Button("Update",new ClickHandler() {
-					
 					@Override
-					public void onClick(ClickEvent event) {
-						
-						System.out.println(studentgrid.getColumn(0).getValue(result1.get(0)));
+					public void onFailure(Throwable caught) {
+
 					}
+
+					@Override
+					public void onSuccess(List<Student> result) {
+						addResult(result);
+
+					}
+
 				});
-				Button deletebutton=new Button("Delete");
-				h1.add(updatebutton);
-				h1.add(deletebutton);
-				vpanel.add(h1);
-				
-					/*for (Student student : result) {
-						if(studentgrid.getRowContainer().getAbsoluteLeft()){
-							
-						}
-					}*/
-					
-				updatebutton.addClickHandler(new UpdateClickHandler());
-				deletebutton.addClickHandler(new DeleteClickHandler());
-				vpanel.add(deletebutton);
-				
+	}
 
+	public class BackButtonClickHandler implements ClickHandler {
+		@Override
+		public void onClick(ClickEvent event) {
+			MainPage mainpage = new MainPage();
+			// vpanel.add(addStudentpage);
 
+		}
+	}
+
+	public class DeleteClickHandler implements ClickHandler {
+		@Override
+		public void onClick(ClickEvent event) {
+
+		}
+	}
+
+	public class UpdateClickHandler implements ClickHandler {
+		@Override
+		public void onClick(ClickEvent event) {
+
+		}
+	}
+
+	private void addResult(final List<Student> result) {
+		vpanel.clear();
+		VerticalPanel vpanel1 = new VerticalPanel();
+		final DataGrid<Student> studentgrid = new DataGrid<Student>();
+		HorizontalPanel hpanel = new HorizontalPanel();
+		SubmitButton backbutton = new SubmitButton("Back");
+		// backbutton.setWidth("1");
+		backbutton.addClickHandler(new BackButtonClickHandler());
+		hpanel.add(backbutton);
+		vpanel.add(hpanel);
+
+		studentgrid.setRowData(result);
+		Column<Student, Boolean> checkBoxColumn = new Column<Student, Boolean>(
+				new CheckboxCell(false, false)) {
+			@Override
+			public Boolean getValue(Student object) {
+				return null;
 			}
+		};
 
+		EditTextCell nameedit = new EditTextCell();
+		Column<Student, String> namecolumn = new Column<Student, String>(
+				nameedit) {
+
+			@Override
+			public String getValue(Student object) {
+				return object.getName();
+			}
+		};
+		EditTextCell rnedit = new EditTextCell();
+		Column<Student, String> rncolumn = new Column<Student, String>(rnedit) {
+
+			@Override
+			public String getValue(Student object) {
+				return Integer.toString(object.getRollNo());
+			}
+		};
+
+		EditTextCell dobedit = new EditTextCell();
+		Column<Student, String> dobcolumn = new Column<Student, String>(dobedit) {
+
+			@Override
+			public String getValue(Student object) {
+				return object.getDob().toString();
+			}
+		};
+
+		EditTextCell classedit = new EditTextCell();
+		Column<Student, String> classcolumn = new Column<Student, String>(
+				classedit) {
+
+			@Override
+			public String getValue(Student object) {
+				return object.getClass_();
+			}
+		};
+		studentgrid.addColumn(checkBoxColumn, "Select");
+		studentgrid.addColumn(namecolumn, "Name");
+		studentgrid.addColumn(rncolumn, "Roll No");
+		studentgrid.addColumn(dobcolumn, "Date Of Birth");
+		studentgrid.addColumn(classcolumn, "Standard");
+		studentgrid.setSize("400px", "400px");
+
+		HorizontalPanel h1 = new HorizontalPanel();
+		vpanel.remove(vpanel1);
+		vpanel.add(headerPanel);
+		vpanel1 = new VerticalPanel();
+		vpanel1.add(studentgrid);
+		vpanel1.setBorderWidth(1);
+		vpanel.add(vpanel1);
+		Button updatebutton = new Button("Update", new ClickHandler() {
+
+			@Override
+			public void onClick(ClickEvent event) {
+
+				System.out.println(studentgrid.getColumn(0).getValue(
+						result.get(0)));
+			}
 		});
+		Button deletebutton = new Button("Delete");
+		h1.add(updatebutton);
+		h1.add(deletebutton);
+		vpanel.add(h1);
 
-	}
-	public class BackButtonClickHandler implements ClickHandler{
-		@Override
-		public void onClick(ClickEvent event) {
-			// TODO Auto-generated method stub
-			MainPage mainpage=new MainPage();
-			//vpanel.add(addStudentpage);
-			
-		}
-	}
-	public class DeleteClickHandler implements ClickHandler{
-		@Override
-		public void onClick(ClickEvent event) {
-			// TODO Auto-generated method stub
-			
-			
-		}
-	}
-	public class UpdateClickHandler implements ClickHandler{
-		@Override
-		public void onClick(ClickEvent event) {
-			// TODO Auto-generated method stub
-			//MainPage mainpage=new MainPage();
-			//vpanel.add(addStudentpage);
-			
-		}
+		/*
+		 * for (Student student : result) {
+		 * if(studentgrid.getRowContainer().getAbsoluteLeft()){
+		 * 
+		 * } }
+		 */
+
+		updatebutton.addClickHandler(new UpdateClickHandler());
+		deletebutton.addClickHandler(new DeleteClickHandler());
+		vpanel.add(deletebutton);
+
 	}
 
 }
